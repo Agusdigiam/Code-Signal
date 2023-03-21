dbd46e9fc32df6b0733e0423970545665aad9784
Agustin E. Di Giambatista <agustindigiambatista@gmail.com>
2023-03-21 11:03:39 -0300

    Version Final

---
+++ src/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export function Footer() {
+  return (
+    <>
+    <div className='Footer'>
+        <h5>Copyright and all rigths reserved 2023.</h5>
+        <p>Created for Agustin Di Giambatista</p>
+    </div>
+    </>
+  )
+}
+
--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,13 @@ import './index.css'
 import {Middle} from './Middle'
 import {Center} from './Center'
 import './Media-querys.css'
+import {Footer} from './Footer'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <Center />
     <Middle />
+    <Footer />
   </React.StrictMode>,
 )
