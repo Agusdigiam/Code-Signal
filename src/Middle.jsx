96f4298f15ebba229dfd9236b6dab3829e11c990
Agustin Di Giambatista <agustindigiambatista@gmail.com>
2023-03-21 17:20:52 -0300

    Nuevo

--- src/Middle.jsx
+++ src/Middle.jsx
@@ -1,28 +1,34 @@
+import news1 from '../public/assets/news1.jpg'
+import news2 from '../public/assets/news2.jpg'
+import news3 from '../public/assets/news3.jpg'
+import news4 from '../public/assets/news4.jpg'
+
+
 
 export function Middle(props) {
     return( 
         
         <div className="news-container" id="#news">
             <div className="border-card">
-                <img src="./src/assets/news1.jpg" alt="img1" />
+                <img src={news1} alt="img1" />
                 <h5>Lastest News</h5>
                 <p>Lorem ipsum dolor sit amet consectetur</p>
                 <button>Read More...</button>
             </div>
             <div className="border-card">
-                <img src="./src/assets/news2.jpg" alt="img2" />
+                <img src={news2} alt="img2" />
                 <h5>Last Month</h5>
                 <p>Lorem ipsum dolor sit amet consectetur</p>
                 <button>Read More...</button>
             </div>
             <div className="border-card">
-                <img src="./src/assets/news3.jpg" alt="img3" />
+                <img src={news3} alt="img3" />
                 <h5>Last Week</h5>
                 <p>Lorem ipsum dolor sit amet consectetur</p>
                 <button>Read More...</button>
             </div>
             <div className="border-card">
-                <img src="./src/assets/news4.jpg" alt="img4" />
+                <img src={news4} alt="img4" />
                 <h5>Last Day</h5>
                 <p>Lorem ipsum dolor sit amet consectetur</p>
                 <button>Read More...</button>
