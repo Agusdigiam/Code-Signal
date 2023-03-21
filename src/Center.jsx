208a81479c6424875ec5eb44ba21c7f5abd40b78
Agustin E. Di Giambatista <agustindigiambatista@gmail.com>
2023-03-21 10:28:47 -0300

    Version 3.0.0

--- src/Center.jsx
+++ src/Center.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 export function Center() {
   return (
     <>
-    <div className='Img-tittle'><h2>Lastest News About Code...</h2></div>
+
     <img className='Img-center' src="./src/assets/Main-img.jpg"  alt="img1" />
+    
     </>
   );
 }
