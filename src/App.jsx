6acf44e7405f2c31126d56b3a66088534b12c109
Gian vespa <givespas@gmail.com>
2023-03-24 14:02:10 -0500

    feat: use api component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,31 @@
-import React from 'react'
-
+import React, { useState } from 'react';
+import { ApiGetCoin } from './api';
 
 function App(props) {
-  const newTittle = "<Code Signal/>";
+  const [apiData, setApiData] = useState('');
+  const newTittle = '<Code Signal/>';
+  ApiGetCoin().then((res) => setApiData(res.data));
 
   return (
     <>
-    <h1 className='main-Tittle'>{newTittle}</h1>
-    <p className='main-Subtitle'>-Welcome to Code Solutions-</p>
-    <ul className='main-Nav'>
-     <li><a href="#news">News</a></li>
-     <li><a href="#">Post</a></li>
-     <li><a href="#">Questions</a></li>
-     <li><a href="https://github.com/agusdigiam">Contact</a></li>
-    </ul>
+      <h1 className="main-Tittle">{newTittle}</h1>
+      <p className="main-Subtitle">{apiData.msg}</p>
+      <ul className="main-Nav">
+        <li>
+          <a href="#news">News</a>
+        </li>
+        <li>
+          <a href="#">Post</a>
+        </li>
+        <li>
+          <a href="#">Questions</a>
+        </li>
+        <li>
+          <a href="https://github.com/agusdigiam">Contact</a>
+        </li>
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
