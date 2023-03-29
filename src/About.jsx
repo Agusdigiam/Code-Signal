4de8a93ef3fd4f0c9bace61317357d81fe485aa3
Agustin E. Di Giambatista <agustindigiambatista@gmail.com>
2023-03-29 10:48:18 -0300

    Style About

--- src/About.jsx
+++ src/About.jsx
@@ -3,15 +3,17 @@ import agustin from '../public/assets/agustin.jpg'
 import gian from '../public/assets/gian.jpg'
 export function About() {
   return (
-    <div id='2'>
+    <div className='About' id='2'>
         <h2 className='About-title'>About own team!</h2>
-        <h3 className='About-subtitle'>Meet the guys of the Agency...</h3>
-        <div className='AboutCon'>
-            <img className='prof-img' src={agustin} alt="agus"/>
-            <p className='Aboutp'>Hey my name is Agustin  im from Argentina, and i have 2 years solving problems in Mobile Apps creating easy solutions.</p>
-            <img className='prof-img' src={gian} alt="gian" />
-            <p className='Aboutp'>My Name is Gian  im Back-ennd Developer for 10 years of experience in I.T sector.</p>
-        </div>
+  
+        <img className='AboutPhoto' src={agustin} alt="agus"/>
+
+        <p className='Aboutp'>¿Tenes variedad de productos y te interesa que tus clientes puedan verlos? ¿Queres mostrar fotos de tus productos y una descripción de los mismos para así generar mas consultas? Entonces, éste tipo de página es ideal. Incluye un diseño responsive, contenido de hasta 7 secciones, catálogo de productos dividido por categorías, contenido autogestionable</p>
+        
+        <img className='AboutPhoto' src={gian} alt="gian" />
+
+        <p className='Aboutp'>Estudiamos tu marca, evaluamos el target de tus clientes y tus preferencias, para en conjunto llevar a cabo, un diseño de Logo profesional que represente los valores e ideales de tu empresa. En él se definen los parámetros y lineamientos estéticos, para todas las piezas gráficas que se necesiten realizar.</p>
+        
     </div>
   )
 }
